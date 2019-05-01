c90b1a207c3c3d2188ddbabb41bcd3aa8c4df9ab
SgtRhyno73 <02keegan.killian@gulllakecs.org>
2019-05-01 09:22:46 -0700

    New calculator ino file

---
+++ Calculator-code/Calculator-code.ino
@@ -0,0 +1,17 @@
+/*
+ 5/1/19
+ creators: Keegan Killian, and Ben Glaspie
+ We are makeing a simple calculator. 
+ It will add, subtract, divide and multiply
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
